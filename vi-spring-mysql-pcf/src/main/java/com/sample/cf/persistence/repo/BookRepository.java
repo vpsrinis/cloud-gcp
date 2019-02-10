827cf3c1616d8bee961dd7718e4d378e7b0a7796
VPS <42943411+vpsrinis@users.noreply.github.com>
2019-02-10 20:40:00 +0530

    Add files via upload

---
+++ vi-spring-mysql-pcf/src/main/java/com/sample/cf/persistence/repo/BookRepository.java
@@ -0,0 +1,12 @@
+package com.sample.cf.persistence.repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.sample.cf.persistence.model.Book;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+    List<Book> findByTitle(String title);
+}
