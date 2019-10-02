cbf8d1e0f9f774fdf4ecfe52ffbdbe40a7088264
VPS <vpsrinis@gmail.com>
2019-10-03 00:06:59 +0530

    new

---
+++ spring-boot-pgsql/src/main/java/com/springboot/undertow/pgsql/SpringBootPGSqlUnderTowApp.java
@@ -0,0 +1,12 @@
+package com.springboot.undertow.pgsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPGSqlUnderTowApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPGSqlUnderTowApp.class, args);
+	}
+}
